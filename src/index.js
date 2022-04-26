87659b1f0c7aefa042e965a962a8c476cd0878e8
unknown <jvfernandes.contato@gmail.com>
2022-04-26 10:57:08 -0300

    Criando controller de Usuários e sistema de rotas

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 const http = require('http')
 
-const users = require('./mocks/users')
+const routes = require('../routes')
 
 const server = http.createServer((request, response) => {
     console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
 
-    if (request.url === '/users' && request.method === 'GET') {
-        response.writeHead(200, { 'Content-Type': 'application/json' })
-        response.end(JSON.stringify(users))
+    const route = routes.find((routeObj) => (
+        routeObj.endpoint === request.url && routeObj.method === request.method
+    ))
+
+    if (route) {
+        route.handler(request, response)
     } else {
         response.writeHead(404, { 'Content-Type': 'text/html' })
         response.end(`Cannot ${request.method} ${request.url}`)
     }
-
-    // response.writeHead(200, { 'Content-Type': 'text/html' })
-    // response.end('<h1>Ola mundo</h1>')
 })
 
 server.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'))
